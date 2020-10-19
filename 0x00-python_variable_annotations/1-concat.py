475b5349fb56a78d98f8396e70d4fc3d504a8614
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-10-19 12:42:35 -0500

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def concat(str1: str, str2: str) -> str:
+    # type-annotated function concat that takes a string str1 and a string
+    # str2 as arguments and returns a concatenated string
+    return str1 + str2
