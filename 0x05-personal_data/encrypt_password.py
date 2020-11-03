f9fca9a87b774794db02b69a4abfe6b3dbb7de18
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-02 21:51:52 -0500

    Task 6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ def hash_password(password: str) -> bytes:
     """ expects one string argument name password and returns a salted,
         hashed password, which is a byte string. """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ expects 2 arguments and returns a boolean. """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
