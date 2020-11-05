8bdfba9c58b6553729c7921bf28f06d152822fc2
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-04 19:21:11 -0500

    Task 2

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -20,6 +20,12 @@ def unauthorized() -> str:
     abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ endpoint to testing (forbidden) 403 error handler """
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
