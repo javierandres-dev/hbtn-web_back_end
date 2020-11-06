08aa60df7e09768c3a99818421d88efdb644847d
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-06 12:32:29 -0500

    Task 10

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Basic auth, Basic - Base64 part, Basic - Base64 decode,
-    Basic - User credentials """
+    Basic - User credentials, Basic - User object """
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -50,3 +52,18 @@ class BasicAuth(Auth):
             return None, None
         credentials = decoded_base64_authorization_header.split(':', 1)
         return credentials[0], credentials[1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """ returns the User instance based on his email and password """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
