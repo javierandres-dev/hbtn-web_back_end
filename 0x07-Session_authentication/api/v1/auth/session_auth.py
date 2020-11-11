bb413ace44ce9034b30a6185fb3949c103b2bf7a
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-11 11:39:30 -0500

    Task 6

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,11 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value """
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return
+        _id = self.user_id_for_session_id(session_cookie)
+        return User.get(_id)
