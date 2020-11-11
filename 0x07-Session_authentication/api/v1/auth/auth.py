53083604c5bc0378c340083d6ffe6384a65e06e1
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-11 11:18:59 -0500

    Task 4

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """ Auth class, Require auth with stars """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth():
@@ -32,3 +33,10 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user """
         return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request """
+        if request is None:
+            return None
+        session_name = getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
