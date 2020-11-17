7dec537e0434eb1cacad61926e6f3351ff0007ba
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-17 17:06:46 -0500

    Task 6

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, jsonify
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root() -> str:
+    """ return a JSON """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
