9ace4b826d4d6a8641ad1040bdbcb234b53fd9f0
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-17 15:44:57 -0500

    Task 2

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-""" create user """
+""" Create user, Find user """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from typing import TypeVar
 from user import Base, User
 
@@ -29,4 +31,15 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs: dict) -> object:
+        """ takes in arbitrary keyword arguments and returns the first row
+            found in the users table as filtered by the method’s input
+            arguments """
+        if kwargs is None:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
         return user
