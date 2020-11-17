7da982f06bbbdf57b4b38c2f730e279a938f9b21
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-17 16:09:30 -0500

    Task 4

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Hash password """
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """ takes in a password string arguments and returns a string
+        The returned string is a salted hash of the input password,
+        hashed with bcrypt.hashpw """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
