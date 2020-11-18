6d1de82e1830db84989da5d7a03082476797c06e
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-18 11:32:13 -0500

    Task 17

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Basic Flask app, Register user, Log in, Log out, User profile """
+""" Basic Flask app, Register user, Log in, Log out, User profile,
+    Get reset passwords token """
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 app = Flask(__name__)
@@ -56,6 +57,16 @@ def profile():
     if session_id is None or user is None:
         abort(403)
     return jsonify({"email": user.email}), 200
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ generate a token and respond with a 200 HTTP status """
+    email = request.form.get('email')
+    token = AUTH.get_reset_password_token(email)
+    if token is None or email is None:
+        abort(403)
+    return jsonify({"email": email, "reset_token": token}), 200
 
 
 if __name__ == "__main__":
