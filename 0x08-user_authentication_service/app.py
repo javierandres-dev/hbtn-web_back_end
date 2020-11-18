77407b713882d0bb0785e15e4b3ba76e259c8b06
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-18 10:52:05 -0500

    Task 14

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" Basic Flask app, Register user, Log in """
-from flask import Flask, jsonify, request, abort
+""" Basic Flask app, Register user, Log in, Log out """
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
@@ -35,6 +35,17 @@ def login():
     response = jsonify({'email': email, 'message': 'logged in'})
     response.set_cookie('session_id', session_id)
     return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Log out """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
