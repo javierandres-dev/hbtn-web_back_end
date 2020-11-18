1b7d0d0e4dd9722725542162349b71a621565b4b
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-18 10:03:06 -0500

    Task 9

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-""" Hash password, Register user, Credentials validation """
+""" Hash password, Register user, Credentials validation, Generate UUIDs """
 import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 from db import DB
 from user import User
 
@@ -11,6 +12,11 @@ def _hash_password(password: str) -> str:
         The returned string is a salted hash of the input password,
         hashed with bcrypt.hashpw """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ return a string representation of a new UUID """
+    return str(uuid4())
 
 
 class Auth:
