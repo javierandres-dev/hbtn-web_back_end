22975dd40afd3d07d20e9358276b934976fbff5d
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-18 10:56:45 -0500

    Task 14

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -42,7 +42,7 @@ def logout():
     """ Log out """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if not user:
+    if user is None:
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect('/')
