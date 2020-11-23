17232245da5ae84ddce92c015ffceab115af64bc
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-23 13:33:15 -0500

    Task 3

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" Parameterize a unit test, Mock HTTP calls """
+""" Parameterize a unit test, Mock HTTP calls, Parameterize and patch """
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -43,3 +43,26 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value = test_payload
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ TESTCASE """
+    def test_memoize(self):
+        """ Test that when calling a_property twice, the correct result is
+            returned but a_method is only called once using assert_called_once
+        """
+        class TestClass:
+            """ class """
+            def a_method(self):
+                """ method  """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ property """
+                return self.a_method()
+        with patch.object(TestClass, "a_method") as mockMethod:
+            test_class = TestClass()
+            test_class.a_property
+            test_class.a_property
+            mockMethod.assert_called_once
