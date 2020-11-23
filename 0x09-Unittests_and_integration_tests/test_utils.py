b02090f4db12a20b7044eb1784b741f6d7de31d4
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-23 12:42:28 -0500

    Task 0

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Parameterize a unit test """
 import unittest
-from utils import access_nested_map
 from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
