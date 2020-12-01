7753ee090bb1af065b2a36171aec6db6f927925e
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-01 11:21:22 -0500

    Task 4

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Writing strings to Redis, Reading from Redis and recovering original type,
-    Incrementing values, Storing lists """
+    Incrementing values, Storing lists, Retrieving lists """
 from typing import Union, Callable, Optional, Any
 import redis
 import uuid
