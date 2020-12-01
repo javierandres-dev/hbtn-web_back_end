37499eb704be4870e8622d4c4373f8ee38f59bb0
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-01 10:14:35 -0500

    Task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Writing strings to Redis, Reading from Redis and recovering original type
 """
-from redis.client import Redis
 from typing import Union, Callable, Optional, Any
-from uuid import uuid4
+import redis
+import uuid
 
 
 class Cache:
@@ -11,13 +11,13 @@ class Cache:
     def __init__(self):
         """ constructor - store an instance of the Redis client as a private
         variable named _redis and flush the instance using flushdb """
-        self._redis = Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ generate a random key (e.g. using uuid), store the input data in
         Redis using the random key and return the key """
-        key = str(uuid4())
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
