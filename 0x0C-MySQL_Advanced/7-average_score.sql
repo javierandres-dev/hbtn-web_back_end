bbd54ac82b19d5943da34ee2cbc763663fa2823e
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-04 18:50:22 -0500

    Task 7

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student
+DELIMITER |
+CREATE PROCEDURE ComputeOverallScoreForUser (
+    IN user_id int
+)
+BEGIN
+    UPDATE users
+    SET overall_score = (
+        SELECT AVG(score) FROM corrections
+        WHERE corrections.user_id = user_id
+        GROUP BY corrections.user_id
+    )
+    WHERE id = user_id;
+END;
+|
