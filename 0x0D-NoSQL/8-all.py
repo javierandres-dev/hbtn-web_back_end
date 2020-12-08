723067359af12c68360300b972b1ab5ef5207d71
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-08 13:57:42 -0500

    Task 8

---
+++ 0x0D-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+""" List all documents in Python """
+import pymongo
+
+
+def list_all(mongo_collection):
+    """ lists all documents in a collection """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
