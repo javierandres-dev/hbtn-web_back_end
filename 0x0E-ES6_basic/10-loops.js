fd93f2c20c8c87a329af963480065d4f342172ca
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-10 17:42:55 -0500

    Task 10

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let idx of array) {
-    const value = array[idx];
+    let value = array[idx];
     array[idx] = appendString + value;
   }
 
