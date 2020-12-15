20354d0f41da0118b8370234d4c58f7ccd9d9295
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-15 13:35:52 -0500

    Task 2

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,12 +1,9 @@
-// return an object, or an empty Error object
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
       status: 200,
       body: 'success',
     }))
-    .catch(() => new Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
