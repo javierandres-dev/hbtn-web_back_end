c6236f6234fe5cb52ad30b31bf1e55cf65d72cb4
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-15 15:28:19 -0500

    Task 6

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,9 +1,11 @@
 import signUpUser from './4-all-reject';
 import uploadPhoto from './5-all-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled(signUpUser(firstName, lastName), uploadPhoto(fileName)).then((data) => data.map((datum) => ({
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const arr = await Promise.all(signUpUser(firstName, lastName), uploadPhoto(fileName))
+    .then((data) => data.map((datum) => ({
       status: datum.status,
       value: datum.value,
-  })));
+    })));
+  return arr;
 }
