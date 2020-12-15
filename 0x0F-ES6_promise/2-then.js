6085a23bd1fc979fdbc44d1ba5a45ede39825bba
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-15 14:37:07 -0500

    Task 2

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,9 +1,11 @@
-export default function handleResponseFromAPI(promise) {
+function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
       status: 200,
       body: 'success',
     }))
-    .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
+
+export default handleResponseFromAPI;
