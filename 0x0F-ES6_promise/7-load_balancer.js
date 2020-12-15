cd142e22a87705345e31285aaa4b0caab386bf02
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-15 17:07:06 -0500

    Task 7

--- 0x0F-ES6_promise/7-load_balancer.js
+++ 0x0F-ES6_promise/7-load_balancer.js
@@ -1,7 +1,3 @@
-// return the value returned by the promise that resolved the first
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload])
-    .then((value) => {
-      console.log(value);
-    });
+  return Promise.race([chinaDownload, USDownload]);
 }
