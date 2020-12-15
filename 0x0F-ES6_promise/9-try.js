9d39b0b3ce09f62c72667f2e4e3534f97ce0b1f0
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-15 17:19:55 -0500

    Task 9

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -3,11 +3,13 @@ export default function guardrail(mathFunction) {
   let value = undefined;
   try {
     value = mathFunction();
-  } catch (error) {
-    value = error.toString();
-  } finally {
-    queue.push(value);
   }
-  queue.push('Guardrail was processed');
-  return queue;
+  catch (error) {
+    value = error.toString();
+  }
+  finally {
+    queue.push(value);
+    queue.push('Guardrail was processed');
+    return queue;
+  }
 }
