5f5955eb2234fa5a8f9a73ced205e0d08a89b340
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-15 13:51:39 -0500

    Task 3

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,13 +1,9 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((photo, user) => {
-      console.log(
-        photo.body,
-        user.firstName,
-        user.lastName,
-      );
+      console.log(photo.body, user.firstName, user.lastName);
     })
     .catch(() => console.log('Signup system offline'));
 }
