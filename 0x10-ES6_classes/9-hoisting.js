a2d46625bd095dc31178e6ea8fe0010ca7b82764
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-21 12:49:11 -0500

    Task 9

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -38,4 +38,6 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export default const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export listOfStudents;
