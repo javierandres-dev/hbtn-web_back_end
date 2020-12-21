943fa08e04358fa0297e6024992a686ace063c3d
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-21 11:54:48 -0500

    Task 2

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -3,29 +3,35 @@ export default class HolbertonCourse {
     if (typeof name !== 'string') throw TypeError('name must be a string');
     if (typeof length !== 'number') throw TypeError('length must be a number');
     if (students.constructor !== Array && students.every((el) => typeof el === 'string')) {
-      throw TypeError('students must be an Array of Strings');
+      throw TypeError('students must be an array of strings');
     }
     this._name = name;
     this._length = length;
     this._students = students;
   }
+
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
+
   set name(newName) {
     if (typeof newName !== 'string') throw TypeError('name must be a string');
     this._name = newName;
   }
+
   set length(newLength) {
     if (typeof newLength !== 'number') throw TypeError('length must be a number');
     this._length = newLength;
   }
+
   set students(newStudents) {
     if (newStudents.constructor !== Array && newStudents.every((el) => typeof el === 'string')) {
       throw TypeError('students must be an array of strings');
