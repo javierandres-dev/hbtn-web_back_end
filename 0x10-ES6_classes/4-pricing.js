fec604530768c882193fcd769d92158a286699b3
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-21 12:30:39 -0500

    Task 4

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,29 +1,29 @@
-import Currency from './3-currency';/* eslint-disable */
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
+  set amount(amount) {
+    if (typeof amount === 'number') this._amount = amount;
+  }
+
   get currency() {
     return this._currency;
   }
 
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
-
-  set currency(newCurrency) {
-    this._currency = newCurrency;
+  set currency(currency) {
+    if (currency instanceof Currency) this._currency = currency;
   }
 
   displayFullPrice() {
-    return `${this.amount} (${this.currency.displayFullCurrency()})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
