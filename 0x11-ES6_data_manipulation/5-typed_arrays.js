06d63a8d37d41a5e3908b0ca1d2bc176a73410d4
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-23 20:18:04 -0500

    Task 5

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) throw Error('Position outside range');
   const buffer = new ArrayBuffer(length);
-  const int8BufferArray = new Int8Array(buffer);
-  int8BufferArray[position] = value;
-  return int8BufferArray;
+  const view = new DataView(buffer, 0);
+  if (position > length - 1) throw Error('Position outside range');
+  view.setInt8(position, value);
+  return view;
 }
