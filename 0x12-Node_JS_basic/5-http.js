ebd68057b5f47cc7115700a05e3e83725240069b
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-09 17:34:33 -0500

    Task 5

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -4,22 +4,19 @@ const countStudents = require('./3-read_file_async');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
+  if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   }
-  if (req.method === 'GET' && req.url === '/students') {
+  if (req.url === '/students') {
     countStudents(String(process.argv.slice(2)))
       .then((subjects) => {
         res.write('This is the list of our students\n');
-        res.write(`Number of students: ${subjects.count}\n`);
-        for (const subject in subjects) {
-          if (subject && subject !== 'count') res.write(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}\n`);
-        }
+        res.write(`${subjects}`);
         res.end();
       })
-      .catch((error) => { throw error; });
+        .catch((error) => { throw error; });
   }
 });
 app.listen(port);
