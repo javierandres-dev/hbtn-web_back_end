d65d16c13763b671e221b215be2df5eb68fffc98
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-11 17:04:18 -0500

    Task 7

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -4,8 +4,7 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Holberton School!');
-
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.get('/students', async (req, res) => {
   const msg = 'This is the list of our students\n';
   try {
