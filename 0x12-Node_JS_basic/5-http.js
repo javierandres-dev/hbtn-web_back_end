fa0accf99d99190545c429004916812d5515e3dd
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-11 16:19:09 -0500

    Task 5

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -6,18 +6,13 @@ const port = 1245;
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.writeHead(200);
-    res.end('Hello Holberton School!');
-  }
+  if (req.url === '/') res.write('Hello Holberton School!');
   if (req.url === '/students') {
-    res.writeHead(200);
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(String(process.argv.slice(2)));
-      res.end(`${students.join('\n')}`);
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
     } catch (error) {
-      res.writeHead(404);
       res.end(error.message);
     }
   }
