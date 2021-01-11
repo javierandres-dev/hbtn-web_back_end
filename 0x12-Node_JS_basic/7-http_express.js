8eafb7c9a720d31d949c3d0d8f2e03d0d129950b
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-11 16:28:11 -0500

    Task 7

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
+
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
