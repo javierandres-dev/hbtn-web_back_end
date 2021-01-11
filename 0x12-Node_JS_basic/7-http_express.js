219c3ab5d40098a15355d5cd9b65700846aeca13
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-11 16:38:26 -0500

    Task 7

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -4,17 +4,20 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-app.get('/students', ((req, res) => {
- countStudents(String(process.argv.slice(2)))
+app.get('/', (req, res) => res.send('Hello Holberton School!');
+
+app.get('/students', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  countStudents(process.argv[2])
     .then((data) => {
-      res.write('This is the list of our students\n');
-      res.write(`${data}`);
-      res.end();
+      res.end(data);
     })
-    .catch((error) => { 
+    .catch((error) => {
       res.end(error.message);
     });
-}));
-app.listen(port);
+});
+
+app.listen(port)
+
 module.exports = app;
