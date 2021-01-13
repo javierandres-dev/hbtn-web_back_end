d0052368b7817519c95716e916dff3641a60bbc6
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-13 14:10:11 -0500

    Task 0

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('checks the output', () => {
