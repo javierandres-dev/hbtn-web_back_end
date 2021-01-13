597da9dc144bf951f38d8add34724bc4dc8fe644
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-13 13:56:06 -0500

    Task 0

---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('checks the output', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('negative numbers', () => {
+    assert.equal(calculateNumber(-1, 1), 0);
+    assert.equal(calculateNumber(1, -1), 0);
+    assert.equal(calculateNumber(-1, -1), -2);
+  });
+  it('checks arguments', () => {
+    assert.equal(isNaN(calculateNumber(1)), true);
+    assert.equal(isNaN(calculateNumber()), true);
+  });
+});
