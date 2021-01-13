6ba6b890525b222a73ed57f41ce3ff902627753d
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-13 16:55:30 -0500

    Task 3

---
+++ 0x13-unittests_in_js/utils.js
@@ -0,0 +1,18 @@
+'use strict';
+const Utils = {
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      if (b === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+  }
+}
+module.exports = Utils;
