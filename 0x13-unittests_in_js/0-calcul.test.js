18dc3f73549209d52f0cbe59b0f11d31d23c35fb
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-13 14:26:37 -0500

    Task 0

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,22 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('checks the output', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
   it('negative numbers', () => {
-    assert.equal(calculateNumber(-1, 1), 0);
-    assert.equal(calculateNumber(1, -1), 0);
-    assert.equal(calculateNumber(-1, -1), -2);
+    assert.strictEqual(calculateNumber(-1, 1), 0);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(-1, -1), -2);
   });
   it('checks arguments', () => {
-    assert.equal(isNaN(calculateNumber(1)), true);
-    assert.equal(isNaN(calculateNumber()), true);
+    assert.strictEqual(isNaN(calculateNumber(1)), true);
+    assert.strictEqual(isNaN(calculateNumber()), true);
   });
 });
