3d9dadbf1d339648cf08514338cfaf286c0aca1b
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-13 20:07:36 -0500

    Task 9

--- 0x13-unittests_in_js/9-api/api.test.js
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -20,22 +20,29 @@ describe('basic integration testing', () => {
 
 describe('regex integration testing', () => {
   describe('GET /cart/:id', () => {
-    it('endpoint GET /cart/:id', () => {
-      chai.request('http://localhost:7865')
-        .get('/cart/12')
-        .end((err, res) => {
-          if (err) throw err;
-          chai.expect(res.statusCode).to.equal(200);
-          chai.expect(res.text).to.equal('Payment methods for cart 12');
-        });
-    })
-    it('endpoint GET /cart/:isNaN', () => {
-      chai.request('http://localhost:7865')
-        .get('/cart/anything')
-        .end((err, res) => {
-          if (err) throw err;
-          chai.expect(res.statusCode).to.equal(404);
-        });
-    })
-  })
+    it('endpoint: GET /cart/:id', (done) => {
+      const call = {
+        url: 'http://localhost:7865/cart/12',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
+    });
+  });
+  describe('GET /cart/:isNaN', () => {
+    it('endpoint: GET /cart/:isNaN', (done) => {
+      const call = {
+        url: 'http://localhost:7865/cart/anything',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        chai.expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
 });
+
