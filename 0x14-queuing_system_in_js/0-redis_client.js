f1c199d035845f0371741604ec106db8bed52567
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-18 14:02:14 -0500

    Task 1

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+'use strict';
+import redis from 'redis';
+//const redis = require("redis");
+const client = redis.createClient();
+
+client.on("error", (error) => {
+  if (error) console.log(`Redis client not connected to the server: ${error}`)
+}).on('ready', () => {
+    console.log('Redis client connected to the server');
+});
